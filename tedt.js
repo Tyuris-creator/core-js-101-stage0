cfb3ee0e98ba909f557f53b149c39a0bd005f585
Michail <tyuris@icloud.com>
2025-08-05 20:54:42 +0300

    feat: number tasks started

--- tedt.js
+++ tedt.js
@@ -1,5 +1,6 @@
-function isString(value) {
-  return typeof value === 'string';
+function getAverage(value1, value2) {
+  return Math.floor((value1 + value2) / 2);
 }
 
-console.log(isString(new String('test')))
+
+console.log(getAverage(-3,3))
