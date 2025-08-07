cc560b1ea0662e04a7f20329445671c65b3d73eb
Michail <tyuris@icloud.com>
2025-08-07 21:30:31 +0300

    feat: array tasks started and almost finished

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -233,8 +233,8 @@ function toCsvText(arr) {
  *   [ 0, 1, 2, 3, 4, 5 ] => [ 0, 1, 4, 9, 16, 25 ]
  *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
  */
-function toArrayOfSquares(/* arr */) {
-  throw new Error('Not implemented');
+function toArrayOfSquares(arr) {
+  return arr.map((i) => i ** 2);
 }
 
 /**
@@ -251,8 +251,12 @@ function toArrayOfSquares(/* arr */) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  let sum = 0;
+  return arr.map((el) => {
+    sum += el;
+    return sum;
+  });
 }
 
 /**
@@ -266,8 +270,8 @@ function getMovingSum(/* arr */) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  return arr.filter((_, i) => i % 2 !== 0);
 }
 
 /**
@@ -284,8 +288,12 @@ function getSecondItems(/* arr */) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.reduce((acc, el, i) => {
+    const elements = Array(i + 1).fill(el);
+    acc.push(...elements);
+    return acc;
+  }, []);
 }
 
 /**
@@ -301,8 +309,11 @@ function propagateItemsByPositionIndex(/* arr */) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  return arr
+    .slice()
+    .sort((a, b) => b - a)
+    .slice(0, 3);
 }
 
 /**
@@ -318,8 +329,11 @@ function get3TopItems(/* arr */) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  // return arr.filter((el) => typeof el === 'number' && el > 0).length;
+  return arr.reduce((count, item) => {
+    return typeof item === 'number' && item > 0 ? count + 1 : count;
+  }, 0);
 }
 
 /**
--- tedt.js
+++ tedt.js
@@ -1,6 +1,14 @@
-function getAverage(value1, value2) {
-  return Math.floor((value1 + value2) / 2);
+function propagateItemsByPositionIndex(arr) {
+  let count = 1;
+  return arr.reduce((acc, el) => {
+    console.log(typeof el)
+    const elements = el.repeat(count).split('');
+    count += 1;
+    acc.push(...elements);
+    return acc;
+  }, []);
 }
+console.log(propagateItemsByPositionIndex(['a', 'b']));
 
 
-console.log(getAverage(-3,3))
+console.log([].length)
